2309b612e717597a502647c3ec14d9ee75181a7f
Laura Patron <69320369+patron02@users.noreply.github.com>
2024-06-26 15:40:32 -0500

    Update gui_temp.ps1

--- GUI/gui_temp.ps1
+++ GUI/gui_temp.ps1
@@ -1,4 +1,13 @@
 # Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypas
+<# Synopsis: Creates a new .iso file
+  Description: The New-IsoFile cmdlet creates a new .iso file containing content from chosen folders. Example New-IsoFile "c:\tools","c:Downloads\utils" 
+  This command creates a .iso file in $env:temp folder (default location) that contains c:\tools and c:\downloads\utils folders. 
+  The folders themselves are included at the root of the .iso image. .Example New-IsoFile -FromClipboard -Verbose Before running this command, select and copy (Ctrl-C) files/folders in Explorer first. 
+  Example dir c:\WinPE | New-IsoFile -Path c:\temp\WinPE.iso -BootFile "${env:ProgramFiles(x86)}\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\Oscdimg\efisys.bin" 
+  -Media DVDPLUSR -Title "WinPE" This command creates a bootable .iso file containing the content from c:\WinPE folder, but the folder itself isn't included. 
+  Boot file etfsboot.com can be found in Windows ADK. Refer to IMAPI_MEDIA_PHYSICAL_TYPE enumeration for possible media types: http://msdn.microsoft.com/en-us/library/windows/desktop/aa366217(v=vs.85).aspx 
+  Authors: Chris Wu, Nick Johnson, Laura Patron #> 
+
 
 Add-Type -assembly System.Windows.Forms
 
@@ -26,126 +35,102 @@ function abort-IsoCreation{
     $global:AbortIsoCreation = $true
 }
 
-# Define the ISO function
-function New-IsoFileWithGUI {
-    [CmdletBinding()]
-    Param(
-        [string]$SourceDir,
-        [string]$DestinationDir,
-        [string]$MediaType
-    )
+# Function definition
+function New-IsoFile {   
+    [CmdletBinding(DefaultParameterSetName='Source')]Param( 
+        [string]$global:SourceDir,
+        [string]$global:DestinationDir,
+        [string]$global:MediaType,
+        [parameter(Position=1,Mandatory=$true,ValueFromPipeline=$true, ParameterSetName='Source')] $Source,  
+        [parameter(Position=2)][string]$Path = "$DestinationDir",
+        [ValidateScript({Test-Path -LiteralPath $_ -PathType Leaf})]
+        [string]$BootFile = $null, 
+        [ValidateSet('CDR','CDRW','DVDRAM','DVDPLUSR','DVDPLUSRW','DVDPLUSR_DUALLAYER','DVDDASHR','DVDDASHRW','DVDDASHR_DUALLAYER','DISK','DVDPLUSRW_DUALLAYER','BDR','BDRE')]
+        [string] $Media = 'DVDPLUSRW_DUALLAYER', 
+        [string]$Title = (Get-Date).ToString("yyyyMMdd-HHmmss.ffff"),  
+        [switch]$Force, 
+        [parameter(ParameterSetName='Clipboard')]
+        [switch]$FromClipboard
+    ) 
 
-    # Validate source directory
-    if (-not (Test-Path -Path $SourceDir -PathType Container)) {
-        Show-ErrorDialog "Source directory '$SourceDir' does not exist or is not accessible."
-        # SEND THIS ERROR TO GUI
-        return
-    }
+    Begin {  
+        ($cp = new-object System.CodeDom.Compiler.CompilerParameters).CompilerOptions = '/unsafe' 
+        if (!('ISOFile' -as [type])) {  
+            Add-Type -CompilerParameters $cp -TypeDefinition @'
+            public class ISOFile  
+            { 
+                public unsafe static void Create(string Path, object Stream, int BlockSize, int TotalBlocks)  
+                {  
+                    int bytes = 0;  
+                    byte[] buf = new byte[BlockSize];  
+                    var ptr = (System.IntPtr)(&bytes);  
+                    var o = System.IO.File.OpenWrite(Path);  
+                    var i = Stream as System.Runtime.InteropServices.ComTypes.IStream;  
 
-    # Validate destination directory
-    if (-not (Test-Path -Path $DestinationDir -PathType Container)) {
-        Show-ErrorDialog "Destination directory '$DestinationDir' does not exist or is not accessible."
-        # SEND THIS ERROR TO GUI
-        return
-    }
-
-    # Function definition
-    function New-IsoFile 
-    {   
-        [CmdletBinding(DefaultParameterSetName='Source')]
-        Param( 
-            [parameter(Position=1,Mandatory=$true,ValueFromPipeline=$true, ParameterSetName='Source')]
-            $Source,  
-            [parameter(Position=2)]
-            [string]$Path = "$DestinationDir\$((Get-Date).ToString('yyyyMMdd-HHmmss.ffff')).iso",  
-            [ValidateScript({Test-Path -LiteralPath $_ -PathType Leaf})]
-            [string]$BootFile = $null, 
-            [ValidateSet('CDR','CDRW','DVDRAM','DVDPLUSR','DVDPLUSRW','DVDPLUSR_DUALLAYER','DVDDASHR','DVDDASHRW','DVDDASHR_DUALLAYER','DISK','DVDPLUSRW_DUALLAYER','BDR','BDRE')]
-            [string] $Media = 'DVDPLUSRW_DUALLAYER', 
-            [string]$Title = (Get-Date).ToString("yyyyMMdd-HHmmss.ffff"),  
-            [switch]$Force, 
-            [parameter(ParameterSetName='Clipboard')]
-            [switch]$FromClipboard 
-        ) 
-
-        Begin {  
-            ($cp = new-object System.CodeDom.Compiler.CompilerParameters).CompilerOptions = '/unsafe' 
-            if (!('ISOFile' -as [type])) {  
-                Add-Type -CompilerParameters $cp -TypeDefinition @'
-                public class ISOFile  
-                { 
-                    public unsafe static void Create(string Path, object Stream, int BlockSize, int TotalBlocks)  
-                    {  
-                        int bytes = 0;  
-                        byte[] buf = new byte[BlockSize];  
-                        var ptr = (System.IntPtr)(&bytes);  
-                        var o = System.IO.File.OpenWrite(Path);  
-                        var i = Stream as System.Runtime.InteropServices.ComTypes.IStream;  
-
-                        if (o != null) { 
-                            while (TotalBlocks-- > 0) {  
-                                i.Read(buf, BlockSize, ptr); o.Write(buf, 0, bytes);  
-                            }  
-                            o.Flush(); o.Close();  
-                        } 
+                    if (o != null) { 
+                        while (TotalBlocks-- > 0) {  
+                            i.Read(buf, BlockSize, ptr); o.Write(buf, 0, bytes); 
+                        }  
+                        o.Flush(); o.Close();  
                     } 
-                }  
+                } 
+            }  
 '@  
-            } 
-
-            if ($BootFile) { 
-                if('BDR','BDRE' -contains $Media) { Write-Warning "Bootable image doesn't seem to work with media type $Media" } 
-                ($Stream = New-Object -ComObject ADODB.Stream -Property @{Type=1}).Open()  # adFileTypeBinary 
-                $Stream.LoadFromFile((Get-Item -LiteralPath $BootFile).Fullname) 
-                ($Boot = New-Object -ComObject IMAPI2FS.BootOptions).AssignBootImage($Stream) 
-            } 
-
-            $MediaType = @('UNKNOWN','CDROM','CDR','CDRW','DVDROM','DVDRAM','DVDPLUSR','DVDPLUSRW','DVDPLUSR_DUALLAYER','DVDDASHR','DVDDASHRW','DVDDASHR_DUALLAYER','DISK','DVDPLUSRW_DUALLAYER','HDDVDROM','HDDVDR','HDDVDRAM','BDROM','BDR','BDRE') 
-
-            # Show-InfoMessage "Selected media type is $Media with value $($MediaType.IndexOf($Media))"
-            ($Image = New-Object -com IMAPI2FS.MsftFileSystemImage -Property @{VolumeName=$Title}).ChooseImageDefaultsForMediaType($MediaType.IndexOf($Media)) 
-
-            if (!($Target = New-Item -Path $Path -ItemType File -Force:$Force -ErrorAction SilentlyContinue)) { 
-                Show-ErrorDialog "Cannot create file $Path. Use -Force parameter to overwrite if the target file already exists."; 
-                break 
-            } 
-        }  
-
-        Process { 
-            if($global:AbortIsoCreation){
-                Show-InfoMessage "ISO creation process aborted."
-                return
-            }
-
-            if($FromClipboard) { 
-                if($PSVersionTable.PSVersion.Major -lt 5) { Show-ErrorDialog'The -FromClipboard parameter is only supported on PowerShell v5 or higher'; break } 
-                $Source = Get-Clipboard -Format FileDropList 
-            } 
-
-            foreach($item in $Source) { 
-                if($item -isnot [System.IO.FileInfo] -and $item -isnot [System.IO.DirectoryInfo]) { 
-                    $item = Get-Item -LiteralPath $item
-                } 
-
-                if($item) { 
-                    Write-Verbose -Message "Adding item to the target image: $($item.FullName)"
-                    try { $Image.Root.AddTree($item.FullName, $true) } catch { Show-ErrorDialog ($_.Exception.Message.Trim() + ' Try a different media type.') } 
-                } 
-            } 
         } 
 
-        End {  
-            if ($Boot) { $Image.BootImageOptions=$Boot }  
-            $Result = $Image.CreateResultImage()  
-            [ISOFile]::Create($Target.FullName,$Result.ImageStream,$Result.BlockSize,$Result.TotalBlocks) 
-            Show-InfoMessage "Target image ($($Target.FullName)) has been created"
-            $Target
+        if ($BootFile) { 
+            if('BDR','BDRE' -contains $Media) { Write-Warning "Bootable image doesn't seem to work with media type $Media" } 
+            ($Stream = New-Object -ComObject ADODB.Stream -Property @{Type=1}).Open()  # adFileTypeBinary 
+            $Stream.LoadFromFile((Get-Item -LiteralPath $BootFile).Fullname) 
+            ($Boot = New-Object -ComObject IMAPI2FS.BootOptions).AssignBootImage($Stream) 
+        } 
+
+        $MediaType = @('UNKNOWN','CDROM','CDR','CDRW','DVDROM','DVDRAM','DVDPLUSR','DVDPLUSRW','DVDPLUSR_DUALLAYER','DVDDASHR','DVDDASHRW','DVDDASHR_DUALLAYER','DISK','DVDPLUSRW_DUALLAYER','HDDVDROM','HDDVDR','HDDVDRAM','BDROM','BDR','BDRE') 
+
+        # Show-InfoMessage "Selected media type is $Media with value $($MediaType.IndexOf($Media))"
+        ($Image = New-Object -com IMAPI2FS.MsftFileSystemImage -Property @{VolumeName=$Title}).ChooseImageDefaultsForMediaType($MediaType.IndexOf($Media)) 
+
+        if (!($Target = New-Item -Path $Path -ItemType File -Force:$Force -ErrorAction SilentlyContinue)) { 
+            Show-ErrorDialog "Cannot create file $path. Use -Force parameter to overwrite if the target file already exists."; 
+            break 
+        } 
+    }  
+
+    Process { 
+        if($global:AbortIsoCreation){
+            Show-InfoMessage "ISO creation process aborted."
+            return
+        }
+
+        if($FromClipboard) { 
+            if($PSVersionTable.PSVersion.Major -lt 5) { Show-ErrorDialog'The -FromClipboard parameter is only supported on PowerShell v5 or higher'; break } 
+            $Source = Get-Clipboard -Format FileDropList 
+        } 
+
+    foreach($item in $Source) { 
+        if($item -isnot [System.IO.FileInfo] -and $item -isnot [System.IO.DirectoryInfo]) { 
+            $item = Get-Item -LiteralPath $item
+        } 
+
+        if($item) { 
+            Write-Verbose -Message "Adding item to the target image: $($item.FullName)"
+            try { $Image.Root.AddTree($item.FullName, $true) 
+            } 
+            catch { 
+                Show-ErrorDialog ($_.Exception.Message.Trim() + ' Try a different media type.') 
+            } 
         } 
     } 
+} 
 
-    $source_dir = $SourceDir
-    New-ISOFile -Source $source_dir -Path $DestinationDir -Media $MediaType
-}
+    End {  
+        if ($Boot) { $Image.BootImageOptions=$Boot }  
+        $Result = $Image.CreateResultImage()  
+        [ISOFile]::Create($Target.FullName,$Result.ImageStream,$Result.BlockSize,$Result.TotalBlocks) 
+        Show-InfoMessage "Target image ($($Target.FullName)) has been created"
+        $Target
+    } 
+} 
 
 # Create the form
 
@@ -224,7 +209,7 @@ $form.Controls.Add($btnBrowseDestination)
 # Create media type dropdown
 $cmbMediaType = New-Object System.Windows.Forms.ComboBox
 $cmbMediaType.Left = 150
-$cmbMediaType.Top = 80
+$cmbMediaType.Top = 120
 $cmbMediaType.Width = 250
 $cmbMediaType.Items.AddRange(@("CDR","CDRW","DVDRAM","DVDPLUSR","DVDPLUSRW","DVDPLUSR_DUALLAYER","DVDDASHR","DVDDASHRW","DVDDASHR_DUALLAYER","DISK","DVDPLUSRW_DUALLAYER","BDR","BDRE"))
 $form.Controls.Add($cmbMediaType)
@@ -233,53 +218,61 @@ $form.Controls.Add($cmbMediaType)
 $LabelMedia = New-Object System.Windows.Forms.Label
 $LabelMedia.Text = "Media Type:"
 $LabelMedia.Left = 10 
-$LabelMedia.Top = 80
+$LabelMedia.Top = 120
 $form.Controls.Add($LabelMedia)
+
+# Create a label for filename
+$labelFile = New-Object System.Windows.Forms.Label
+$labelFile.Text= "Desired File Name:"
+$labelFile.Left = 10
+$labelFile.Top = 80
+$labelFIle.Width = 130
+$form.Controls.Add($labelFile)
+
+# Create a textbox for File Input
+$textboxFile = New-Object System.Windows.Forms.TextBox
+$textboxFile.Left = 150
+$textboxFile.Width = 250
+$textboxFile.Top = 80
+$form.Controls.Add($textboxFile)
+
+# Concatenate file name
+$textboxFile.Add_Leave({
+    $script:filename = $textboxFile.Text.Trim() 
+    if (-not [string]::IsNullOrWhiteSpace($filename)) {
+        $script:filename += ".iso"
+        $script:filename = $script:filename.tostring()
+    }
+})
 
 # Create button to start ISO creation
 $btnCreateISO = New-Object System.Windows.Forms.Button
 $btnCreateISO.Left = 150
-$btnCreateISO.Top = 120
+$btnCreateISO.Top = 160
 $btnCreateISO.Width = 100
 $btnCreateISO.Text = "Create ISO"
 $btnCreateISO.Add_Click({
     # Reset abort flag
     $global:AbortIsoCreation = $false 
     $sourceDir = $txtSource.Text
-    $destinationDir = $txtDestination.Text
+    $destpath = $txtDestination.Text
+    $destinationDir = Join-Path -Path $destpath -ChildPath $script:filename
     $mediaType = $cmbMediaType.SelectedItem
+    get-childitem $SourceDir | New-ISOFile -Path $DestinationDir
 
-    New-IsoFileWithGUI -SourceDir $sourceDir -DestinationDir $destinationDir -MediaType $mediaType
 })
 $form.Controls.Add($btnCreateISO)
-
-# Progress Bar
-$progressBar = New-Object System.Windows.Forms.ProgressBar
-$progressBar.Style = 'Continuous'
-$ProgressBar.Left = 150
-$ProgressBar.Top = 160
-$progressBar.Width = 250
-$form.Controls.Add($progressBar)
-
-# Select Progress bar
-$LabelProgress = New-Object System.Windows.Forms.Label
-$LabelProgress.Text = "Progress:"
-$LabelProgress.Left = 10 
-$LabelProgress.Top = 160
-$form.Controls.Add($LabelProgress)
 
 # Create abort button
 $buttonAbort = New-Object system.Windows.Forms.Button
 $buttonAbort.Left = 270
-$buttonAbort.Top = 120
+$buttonAbort.Top = 160
 $buttonAbort.width = 100
 $buttonabort.Text = "Abort"
 $buttonAbort.Add_Click({
-    Abort-IsoCreation
-    
+    Abort-IsoCreation 
 })
 $form.Controls.Add($buttonAbort)
-
 
 # Show the form
 $form.ShowDialog() | Out-Null
